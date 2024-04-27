ebf1439abd34cad9b4957f93539e9f1a3bc7a31a
zhushiwei888 <411654110@qq.com>
2024-04-27 15:48:59 +0800

    Create a.js

---
+++ a.js
@@ -0,0 +1,10 @@
+
+        var adjustConfig = new AdjustConfig("rbuixr642oe8", AdjustConfig.EnvironmentProduction);
+
+        adjustConfig.setAttributionCallbackListener(function(attribution) {
+            console.log("Attribution callback received.");
+            console.log("Tracker token = " + attribution.trackerToken);
+            console.log("AdNetwork = " + attribution.network);
+            console.log("Campaign = " + attribution.campaign);
+        });
+        Adjust.create(adjustConfig);
